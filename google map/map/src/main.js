602cfabbb44693aa67654ad3b14c0aa71ff27b85
arwindsingh82 <arwindsingh82@gmail.com>
2020-12-01 09:51:34 +0530

    First commit

---
+++ google map/map/src/main.js
@@ -0,0 +1,23 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import VueGeolocation from 'vue-browser-geolocation'
+
+Vue.config.productionTip = false
+
+Vue.use(VueGeolocation)
+
+import * as VueGoogleMaps from 'vue2-google-maps'
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: ''
+  }
+})
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>'
+})
